1dce2d7fea2f3f03ebc4de7c56308a898724be7a
Bryan Quintana <bryandise@gmail.com>
2023-09-29 09:13:22 -0400

    .

--- src/hooks/useAnimeData.js
+++ src/hooks/useAnimeData.js
@@ -3,12 +3,14 @@ export function useAnimeData(API_URL) {
   const [animes, setAnimes] = useState([]);
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((response) => {
-        const animes = response.data;
-        setAnimes(animes);
-      });
+    setTimeout(() => {
+      fetch(API_URL)
+        .then((response) => response.json())
+        .then((response) => {
+          const animes = response.data;
+          setAnimes(animes);
+        });
+    }, 1200);
   }, [API_URL]);
 
   return animes;
