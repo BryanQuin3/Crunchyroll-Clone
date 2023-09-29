ccbdaa68a0edf7e16a62ecebe7764e50a7c01fb3
Bryan Quintana <bryandise@gmail.com>
2023-09-29 09:38:55 -0400

    .

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,10 +1,37 @@
 /* eslint-disable react/prop-types */
-import { usePngToWebpConverter } from "../hooks/usePngToWebpConverter";
+import { useEffect, useState } from "react";
+
+function convertPngToWebp(pngSrc, callback) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  const img = new Image();
+  img.src = pngSrc;
+
+  img.onload = () => {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    context.drawImage(img, 0, 0, img.width, img.height);
+
+    canvas.toBlob((webpBlob) => {
+      const webpUrl = URL.createObjectURL(webpBlob);
+      callback(webpUrl);
+    }, "image/webp");
+  };
+}
+
 export const AnimeCard = ({ anime }) => {
   const titleOfAnime = anime.title;
   const pngSrc = anime.images?.jpg?.large_image_url;
-  const webpSrc = usePngToWebpConverter(pngSrc);
   const gender = anime.genres[0]?.name || "Generic";
+  const [webpSrc, setWebpSrc] = useState(null);
+
+  useEffect(() => {
+    if (pngSrc) {
+      convertPngToWebp(pngSrc, (webpUrl) => {
+        setWebpSrc(webpUrl);
+      });
+    }
+  }, [pngSrc]);
 
   return (
     <article className="anime-container">
@@ -13,9 +40,19 @@ export const AnimeCard = ({ anime }) => {
           <img src="./img/play.png" alt="" className="play-icon" />
         </a>
         {webpSrc ? (
-          <img loading="lazy" className="cover-img" src={webpSrc} alt="" />
+          <img
+            loading="lazy"
+            className="cover-img"
+            src={webpSrc}
+            alt={titleOfAnime}
+          />
         ) : (
-          <img loading="lazy" className="cover-img" src={pngSrc} alt="" />
+          <img
+            loading="lazy"
+            className="cover-img"
+            src={pngSrc}
+            alt={titleOfAnime}
+          />
         )}
       </figure>
       <p className="anime-title">{titleOfAnime}</p>
