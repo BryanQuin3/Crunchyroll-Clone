672f96623e7d05b395630efadfc99209e19608a2
Bryan Quintana <bryandise@gmail.com>
2023-10-08 15:29:01 -0300

    .

--- src/constants/searchAnimeFromMocks.js
+++ src/constants/searchAnimeFromMocks.js
@@ -1,6 +1,6 @@
 import recommendedAnimes from "../mocks/recommended.json";
 
 export const searchAnimeFromMocks = (id) => {
-  const anime = recommendedAnimes.find((item) => item.mal_id == id);
+  const anime = recommendedAnimes.data.find((item) => item.mal_id == id);
   if (anime) return anime;
 };
