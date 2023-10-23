2cba2420fe6faa2ff6e55ed4e45eafcd69e15772
Bryan Quintana <bryandise@gmail.com>
2023-10-23 20:50:08 -0300

    .

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -5,12 +5,7 @@ export const GenresPage = () => {
       {genres.data.map((genre) => {
         return (
           <figure className="cover-container ripple" key={genre.id}>
-            <img
-              loading="lazy"
-              src={genre["src-cover"]}
-              alt=""
-              className="img-cover"
-            />
+            <img src={genre["src-cover"]} alt="" className="img-cover" />
             <img src={genre["src-icon"]} alt="" className="play-icon" />
             <figcaption>{genre.name}</figcaption>
           </figure>
