e378a9036f9bd410f49083543886fff307d12870
Bryan Quintana <bryandise@gmail.com>
2023-10-26 10:06:05 -0300

    .

--- src/constants/formatedRatingData.js
+++ src/constants/formatedRatingData.js
@@ -1,7 +1,9 @@
 export const formatedRatingData = (score, scored_by) => {
   const rating = Math.floor(score / 2);
   let scored = scored_by;
-  if (scored_by > 1000) {
+  if (scored_by > 1000000) {
+    scored = (scored_by / 1000000).toFixed(2) + "M";
+  } else if (scored_by > 1000) {
     scored = Math.floor(scored_by / 1000) + "k";
   }
   return { rating, scored };
