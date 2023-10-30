f4284d737f8434f09d63fe3d8ac76bb58df8386b
Bryan Quintana <bryandise@gmail.com>
2023-10-30 17:32:50 -0300

    .

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -3,7 +3,7 @@ import { MyListsPage } from "./MyListsPage";
 export const HistoryPage = () => {
   return (
     <MyListsPage history={true}>
-      <h1>Historial</h1>
+      <h1 className="history">Historial</h1>
     </MyListsPage>
   );
 };
