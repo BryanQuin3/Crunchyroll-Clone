172390e808f01f24b12a17044c5f34d01ef4915d
Bryan Quintana <bryandise@gmail.com>
2023-10-30 17:02:03 -0300

    .

--- src/pages/CrunchyListsPage.jsx
+++ src/pages/CrunchyListsPage.jsx
@@ -3,7 +3,11 @@ import { MyListsPage } from "./MyListsPage";
 export const CrunchyListsPage = () => {
   return (
     <MyListsPage crunchylists={true}>
-      <h1>CrunchyLists</h1>
+      <div className="crunchylist-page">
+        <img src="../img/empty-cr-list.webp" alt="empty crunchylist image" />
+        <p>Todavía no tienes ninguna Crunchylista. ¡Vamos a crear una!</p>
+        <button className="btn-crunchy ripple">Crear una nueva lista</button>
+      </div>
     </MyListsPage>
   );
 };
