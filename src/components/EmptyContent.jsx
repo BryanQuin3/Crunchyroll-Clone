fd58b0ec274e828f04b5a5d8e5e436c3d2be4ded
Bryan Quintana <bryandise@gmail.com>
2023-10-31 11:29:33 -0300

    .

--- src/components/EmptyContent.jsx
+++ src/components/EmptyContent.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useViewTransition } from "../hooks/useViewTransition";
+import { Link } from "react-router-dom";
 export const EmptyContent = ({ message, buttonText, srcImage }) => {
+  const { handleClick } = useViewTransition("./");
   return (
     <div className="crunchylist-page">
       <img src={srcImage} alt="empty crunchylist image" />
       <p>{message}</p>
-      <button className="btn-crunchy ripple">{buttonText}</button>
+      <Link to="/" className="btn-crunchy ripple" onClick={handleClick}>
+        {buttonText}
+      </Link>
     </div>
   );
 };
